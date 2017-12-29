f78c35dabdaa9c21100aed077b123d428ae0cf69
Oleksandr Barsuk <oleksandr_barsuk@epam.com>
2017-12-29 23:03:24 +0200

    Added controllers

---
+++ Linguist.Web/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Linguist.Services;
+
+namespace Linguist.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserService _userService;
+
+        [HttpGet]
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string login, string password)
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
