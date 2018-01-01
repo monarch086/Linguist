2121f335bd881d8c9a7e727299f58070c437c77f
Oleksandr Barsuk <oleksandr_barsuk@epam.com>
2018-01-01 20:10:29 +0200

    Added Word- and Category- Controllers

---
+++ Linguist.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Linguist.Services.Interfaces;
+using System.Web.Mvc;
+
+namespace Linguist.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUsersService _userService;
+        private readonly ICategoriesService _categoriesService;
+
+        public CategoryController(IUsersService userService, ICategoriesService categoriesService)
+        {
+            _userService = userService;
+            _categoriesService = categoriesService;
+        }
+
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Linguist.Web/Controllers/WordController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Linguist.DataLayer.Model;
+using Linguist.Services.Interfaces;
+
+namespace Linguist.Web.Controllers
+{
+    public class WordController : Controller
+    {
+        private readonly IUsersService _userService;
+        private readonly IWordsService _wordsService;
+
+        public WordController(IUsersService userService, IWordsService wordsService)
+        {
+            _userService = userService;
+            _wordsService = wordsService;
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(string login, string word, string translation, int categoryId)
+        {
+            User user = _userService.GetUserByLogin(login);
+
+            Word _word = new Word
+            {
+                UserId = user.UserId,
+                OriginalWord = word,
+                Translation = translation,
+                CategoryId = categoryId,
+                DateAdded = DateTime.Now
+            };
+
+            _wordsService.AddWord(_word);
+            return Redirect(Url.Action("MyWords", "Home", new { login }));
+        }
+    }
+}
