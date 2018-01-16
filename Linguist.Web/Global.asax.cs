1fa7ca1bdba0530a5a56d16962ffc6dc5932fa90
Oleksandr Barsuk <oleksandr_barsuk@epam.com>
2018-01-16 20:56:22 +0200

    Added handling of CryptographicException

--- Linguist.Web/Global.asax.cs
+++ Linguist.Web/Global.asax.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Security.Cryptography;
+using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.UI.WebControls;
 
 namespace Linguist.Web
 {
@@ -9,6 +13,15 @@ namespace Linguist.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender_, CommandEventArgs e_)
+        {
+            Exception exception = Server.GetLastError();
+            if (exception is CryptographicException)
+            {
+                FormsAuthentication.SignOut();
+            }
         }
     }
 }
