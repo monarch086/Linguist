a2d74bf10ddaa30b6036d8cb5155765a58877e0a
Oleksandr Barsuk <oleksandr_barsuk@epam.com>
2018-01-21 16:01:29 +0200

    Added TestController and views

---
+++ Linguist.Web/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Linguist.Services.Interfaces;
+
+namespace Linguist.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IAccountsService _accountsService;
+        private readonly IUsersService _userService;
+        private readonly IWordsService _wordsService;
+
+        public TestController(IAccountsService accountsService, IUsersService userService, IWordsService wordsService, ICategoriesService categoriesService)
+        {
+            _userService = userService;
+            _wordsService = wordsService;
+            _accountsService = accountsService;
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult AllWords()
+        {
+            var login = _accountsService.GetUserName(System.Web.HttpContext.Current);
+            var words = _userService.GetUserWords(login).ToList();
+
+            var rnd = new Random();
+            words = words.OrderBy(item => rnd.Next()).ToList();
+
+            return View("~/Views/Test/Test.cshtml", words);
+        }
+    }
+}
