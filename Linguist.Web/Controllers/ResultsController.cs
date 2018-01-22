d7c83d2a286cfad8a04f40a41c6ebf50224000a5
Oleksandr Barsuk <oleksandr_barsuk@epam.com>
2018-01-22 22:42:14 +0200

    Added ResultsController

---
+++ Linguist.Web/Controllers/ResultsController.cs
@@ -0,0 +1,29 @@
+﻿using Linguist.Services.Interfaces;
+using System.Web.Mvc;
+
+namespace Linguist.Web.Controllers
+{
+    public class ResultsController : Controller
+    {
+        private readonly IAccountsService _accountsService;
+        private readonly IUsersService _userService;
+        private readonly IWordsService _wordsService;
+
+        public ResultsController(IAccountsService accountsService, IUsersService userService, IWordsService wordsService)
+        {
+            _accountsService = accountsService;
+            _userService = userService;
+            _wordsService = wordsService;
+        }
+
+        public ActionResult Statistics()
+        {
+            var login = _accountsService.GetUserName(System.Web.HttpContext.Current);
+
+            if (string.IsNullOrEmpty(login))
+                return Redirect(Url.Action("Start", "Account"));
+
+            return View();
+        }
+    }
+}
